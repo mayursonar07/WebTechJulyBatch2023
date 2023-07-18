e52b47e5ff2685e31fe2258023f14b06ae9daaf9
mayurprojects07 <mayurprojects07@gmail.com>
2023-07-18 08:17:07 +0530

    First lecture

--- Lecture1/App.js
+++ Lecture1/App.js
@@ -1,3 +1,63 @@
 console.log("Hello");
 
 console.log("Some new changes");
+
+// Global scope
+var a = 0;
+let b = 20;
+const c = 30;
+
+{
+    // Block scope
+    console.log(a);    
+    console.log(b);
+    console.log(c);
+    var x = 100;
+    let y = 200;
+    const z = 300;
+    {
+        // Block scope
+        b = 500;
+        console.log(b)
+    }
+}
+
+console.log(x);    
+//console.log(y);
+//console.log(z);
+
+
+function sampleFunc () {
+    console.log("My sample function called");
+}
+
+sampleFunc();
+
+function add(a, b) {
+    const sum = a+b;
+    // console.log(sum);
+    return sum;
+}
+
+const sum = add(4, 6);
+console.log(sum);
+
+console.log(add(3,6));
+
+// Arrow Functions
+const sampleArrowFunction = () => {
+    console.log("Sample arrow function called..");
+}
+
+sampleArrowFunction();
+
+//
+(function () {
+    console.log("Anonymous function called....");
+})();
+
+// a();
+// How will you call anonymous function?
+// IIFEs : Immediately invoked functions
+
+
---
+++ Lecture1/Hoisting.js
@@ -0,0 +1,13 @@
+console.log("a :", a);
+var a = 10;
+console.log("a :", a);
+
+// // For let type
+console.log("b :", b);
+let b;
+console.log("b :", b);
+
+// For const type
+console.log("c :", c);
+const c = 10;
+console.log("c :", c);
